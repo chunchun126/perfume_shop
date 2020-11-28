2e5f66eefda68810795267648f2ee0a278ce8199
chunchun126 <chun780126@gmail.com>
2020-11-28 16:34:09 +0800

    Update 2020-11-28T08:34:03.566Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,11 +1,13 @@
-AOS.init();
+"use strict";
 
-$(window).scroll(function(e) {
+AOS.init();
+$(window).scroll(function (e) {
   if ($(window).scrollTop() <= 0) {
     $('.navbar').addClass('bg-white');
     $('.navbar').removeClass('bg-light');
-  }else {
+  } else {
     $('.navbar').addClass('bg-light');
     $('.navbar').removeClass('bg-white');
   }
-})
+});
+//# sourceMappingURL=all.js.map
